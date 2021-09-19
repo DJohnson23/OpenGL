779619b19b9344073f268a6f00191594122db53b
DJohnson23 <34072778+DJohnson23@users.noreply.github.com>
2021-09-19 15:06:46 -0400

    Added Textures

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -12,6 +12,7 @@
 #include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
+#include "Texture.h"
 #include "Shader.h"
 
 int main(void) 
@@ -46,10 +47,10 @@ int main(void)
 
     {
         float positions[] = {
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-            -0.5f,  0.5f,
+            -0.5f, -0.5f, 0.0f, 0.0f,
+             0.5f, -0.5f, 1.0f, 0.0f,
+             0.5f,  0.5f, 1.0f, 1.0f,
+            -0.5f,  0.5f, 0.0f, 1.0f
         };
 
         unsigned int indices[] = {
@@ -57,10 +58,14 @@ int main(void)
             2, 3, 0
         };
 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
@@ -70,6 +75,10 @@ int main(void)
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+        Texture texture("res/textures/Apple.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
 
         va.Unbind();
         vb.Unbind();
--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -14,7 +14,7 @@ class Shader
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererID;
-	std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
 
 public:
 	Shader(const std::string& filepath);
@@ -23,8 +23,9 @@ public:
 	void Bind() const;
 	void Unbind() const;
 
-	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniform1i(const std::string& name, int value);
 	void SetUniform1f(const std::string& name, float value);
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 
 private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
